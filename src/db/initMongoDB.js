bf3cf4aed5a43648a4feff01d203f330d06b3fc4
VladyslavKurdijashko <159957674+VladyslavKurdijashko@users.noreply.github.com>
2024-10-28 18:57:45 +0200

    Update initMongoDB.js

--- src/db/initMongoDB.js
+++ src/db/initMongoDB.js
@@ -8,7 +8,7 @@ export const initMongoConnection = async () => {
         const url = env("MONGODB_URL");
         const db = env("MONGODB_DB");
 
-        await mongoose.connect(`mongodb+srv://${user}:${pwd}@${url}/${db}`);
+        await mongoose.connect(`mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`);
 
         console.log("Mongo connection successfully established!");
     } catch (error) {
